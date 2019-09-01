b96f7241ec7653ead698d9a40a576f799b3789dc
Renato Cervati <renato.cervati@gmail.com>
2019-09-01 07:44:31 -0300

    server

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ let app = express()
 let path = require('path')
 
 app.use(express.static(path.join(__dirname)))
+app.use("styles.css", express.static(__dirname))
+app.use("/src", express.static(__dirname + '/src'))
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + 'index.html'));
 })
+
+app.listen(process.env.PORT || 8080)
