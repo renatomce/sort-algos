261492b2553af0d7d87502c52ed23cfc2e10ce92
Renato Cervati <renato.cervati@gmail.com>
2019-09-13 22:28:07 -0300

    deploy

--- server.js
+++ server.js
@@ -1,16 +1,4 @@
-'use strict';
-
 let express = require('express');
 let app = express();
-let path = require('path');
-
-app.use(express.static(path.join(__dirname)));
-app.use("/res/styles.css", express.static(__dirname));
-app.use("/res/hamburgers.css", express.static(__dirname));
-app.use("/dist", express.static(__dirname + '/dist'));
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-})
-
-app.listen(process.env.PORT || 8080);
+app.use(express.static(__dirname + '/dist'));
+app.listen(process.env.PORT || 8081);
