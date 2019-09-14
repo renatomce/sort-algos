fbfccf6b32c968daa96465884a84913bfd33e5b4
Renato Cervati <renato.cervati@gmail.com>
2019-09-13 22:05:20 -0300

    server

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ let path = require('path');
 
 app.use(express.static(path.join(__dirname)));
 app.use("/res/styles.css", express.static(__dirname));
-app.use("/src", express.static(__dirname + '/src'));
+app.use("/res/hamburgers.css", express.static(__dirname));
+app.use("/dist", express.static(__dirname + '/dist'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
