9bec165cdbb581667dfa61f7bf5dbcf34553a1c9
Renato Cervati <renato.cervati@gmail.com>
2019-09-13 23:44:30 -0300

    css

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,9 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
+      }, {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
       }]
     },
     plugins:[
